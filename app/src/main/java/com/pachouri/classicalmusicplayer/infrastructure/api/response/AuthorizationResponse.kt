41eab2b43391fc3b0f3feb1a41c3a3e497064725
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2019-04-18 17:35:30 +0530

    Getter for token

--- app/src/main/java/com/pachouri/classicalmusicplayer/infrastructure/api/response/AuthorizationResponse.kt
+++ app/src/main/java/com/pachouri/classicalmusicplayer/infrastructure/api/response/AuthorizationResponse.kt
@@ -10,11 +10,15 @@ class AuthorizationResponse {
 
     @SerializedName("access_token")
     @Expose
-    private val accessToken: String? = null
+    lateinit var accessToken: String
     @SerializedName("token_type")
     @Expose
     private val tokenType: String? = null
     @SerializedName("expires_in")
     @Expose
     private val expiresIn: Long = 0
+
+    fun getToken(): String {
+        return accessToken
+    }
 }
